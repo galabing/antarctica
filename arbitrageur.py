1db4b9dbc97e72a43a4ccd6fc2dbc22e1f9d89bc
Lin Yang <lingyang@lingyang-macbookpro.local>
2013-05-09 22:08:28 -0700

    fix logger

--- arbitrageur.py
+++ arbitrageur.py
@@ -21,8 +21,7 @@ class Arbitrageur(object):
     self._init_market_watchers()
     self.thread_pool = ThreadPoolExecutor(
         max_workers=len(self.market_watchers))
-    self.arbitrage_detector = ArbitrageDetector(
-        config.marginal_profit_rate_normal)
+    self.arbitrage_detector = ArbitrageDetector()
     self._loop()
 
   def _init_market_watchers(self):
@@ -48,28 +47,30 @@ class Arbitrageur(object):
       logging.info('Received %d order books' % len(order_books))
       opportunities = self.arbitrage_detector.process(order_books)
       logging.info('Detected %d opportunities' % len(opportunities))
-      # TODO: Move this out as an observer of this class.
       for opportunity in opportunities:
-        buy_market = opportunity.buy_market
-        sell_market = opportunity.sell_market
-        buy_price = opportunity.weighted_buy_price / 100.0
-        sell_price = opportunity.weighted_sell_price / 100.0
-        amount = opportunity.amount / 100000000.0
-        pay = opportunity.pay / 100.0
-        paid = opportunity.paid / 100.0
-        profit = paid - pay
-        rate = 0.0
-        # Sometimes the trading amount is so small (eg, 10 satoshis) that
-        # the pay is rounded down to 0 cents.
-        if pay > 0:
-          rate = profit * 100 / pay
-        marginal_rate = 100 * (opportunity.min_sell_price -
-                         opportunity.max_buy_price) / opportunity.max_buy_price
-        logging.info('Opportunity: buy=%.2f sell=%.2f from=%s to=%s amount=%.8f'
-                     ' pay=%.2f paid=%.2f profit=%.2f rate=%.2f%% mrate=%.2f%%'
-                     % (buy_price, sell_price, buy_market, sell_market, amount,
-                        pay, paid, profit, rate, marginal_rate))
+        self._log_opportunity(opportunity)
       sleep(config.sleep_between_rounds_sec)
+
+  def _log_opportunity(self, opportunity):
+    buy_market = opportunity.buy_market
+    sell_market = opportunity.sell_market
+    buy_price = opportunity.weighted_buy_price / 100.0
+    sell_price = opportunity.weighted_sell_price / 100.0
+    amount = opportunity.amount / 100000000.0
+    pay = opportunity.pay / 100.0
+    paid = opportunity.paid / 100.0
+    profit = paid - pay
+    rate = 0.0
+    # Sometimes the trading amount is so small (eg, 10 satoshis) that
+    # the pay is rounded down to 0 cents.
+    if pay > 0:
+      rate = profit * 100 / pay
+    marginal_rate = ((opportunity.min_sell_price - opportunity.max_buy_price) *
+                     100.0) / opportunity.max_buy_price
+    logging.info('Opportunity: buy=%.2f sell=%.2f from=%s to=%s amount=%.8f'
+                 ' pay=%.2f paid=%.2f profit=%.2f rate=%.2f%% mrate=%.2f%%'
+                 % (buy_price, sell_price, buy_market, sell_market, amount,
+                    pay, paid, profit, rate, marginal_rate))
 
 def main():
   parser = argparse.ArgumentParser()
